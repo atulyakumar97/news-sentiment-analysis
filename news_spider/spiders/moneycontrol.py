1447a99be683166abbbb144a4884b2abe7fc9f79
atulyakumar97 <atulyakumar97@gmail.com>
2019-07-11 23:23:20 +0530

    Moneycontrol scraping done similar to economictimes
    Fix Bugs : Stocknames don't appear in some rows

--- news_spider/spiders/moneycontrol.py
+++ news_spider/spiders/moneycontrol.py
@@ -13,9 +13,13 @@ class NewsSpider(scrapy.Spider):
 
     global excelinput
     global threshold
+    global yearfrom
 
     threshold = pd.read_excel('input.xlsx', sheet_name='input')['THRESHOLD']
     threshold = float(threshold.dropna().tolist()[0])
+
+    datefrom = pd.read_excel('input.xlsx', sheet_name='input')['DATEFROM']
+    yearfrom = datefrom.dropna().tolist()[0].year
 
     excelinput = pd.read_excel('input.xlsx', sheet_name='input')['COMPANYNAME']
     excelinput = excelinput.dropna().tolist()
@@ -30,16 +34,95 @@ class NewsSpider(scrapy.Spider):
         companieslist = response.css(".MT10 .bl_12::text").extract()  # Scrape list of companies beginning w/ alphabet
         companieslist = list(map(str.upper, companieslist))  # Company names to uppercase
         companieslisturl = response.css(".MT10 .bl_12").xpath("@href").extract()  # Scrape company URLs
-
         companiesID1 = [i.split(sep="/")[-1] for i in companieslisturl]
-        companiesID2 = [re.search("^[A-Z]*",i).group()for i in companiesID1]
+        companiesID2 = [re.search("^[A-Z]*", i).group()for i in companiesID1]
+        nextjump = []
 
-        # stockname = response.css(".gry10:nth-child(1)::text").extract()[1].strip().split()[1]
-        print(companieslist)
-        print(companieslisturl)
-        print(companiesID1)
-        print(companiesID2)
-        yield response
+        for k in excelinput:
+            if len(get_close_matches(k.upper(), companieslist, cutoff=threshold)) > 0:
+                closest_match = get_close_matches(k.upper(), companieslist, cutoff=threshold)[0]
+                for i, j, l in zip(companieslist, companiesID1, companiesID2):
+                    if closest_match == i:
+                        for year in range(int(yearfrom), int(datetime.now().year)+1):
+                            for pagenum in range(1, 16):
+                                nexturl1 = "https://www.moneycontrol.com/stocks/company_info/stock_news.php?sc_id=+"+j+"&scat=&pageno="+str(pagenum)+"&next=0&durationType=Y&Year="+str(year)+"&duration=1&news_type="
+                                nexturl2 = "https://www.moneycontrol.com/stocks/company_info/stock_news.php?sc_id="+l+"&scat=&pageno="+str(pagenum)+"&next=0&durationType=Y&Year="+str(year)+"&duration=1&news_type="
 
-# datetime.now().year
-#https://www.moneycontrol.com/stocks/company_info/stock_news.php?sc_id=IDF01&scat=&pageno=2&next=0&durationType=Y&Year=2018&duration=1&news_type=
+                                print(nexturl1)
+                                print(nexturl2)
+                                items = NewsSpiderItem()
+                                items['COMPANYNAME'] = i
+
+                                request = scrapy.Request(nexturl1, callback=self.parse_company)
+                                request.meta['items'] = items
+                                yield request
+
+                                request = scrapy.Request(nexturl2, callback=self.parse_company)
+                                request.meta['items'] = items
+                                yield request
+
+    def parse_company(self,response):
+        items = response.meta['items']
+
+        links = response.css(".MT15 .FL a").xpath("@href").extract()
+        article_links = []
+        [article_links.append("https://www.moneycontrol.com"+i) for i in links if i not in article_links]
+        try:
+            items['stockname'] = response.css(".gry10:nth-child(1)::text").extract()[1].strip().split()[1]
+        except:
+            pass
+
+        for link in article_links:
+            request = scrapy.Request(link, callback=self.parse_article)
+            request.meta['items'] = items
+            yield request
+
+    def parse_article(self,response):
+        items = response.meta['items']
+        items['article_link'] = response.request.url
+        items['title'] = response.css(".artTitle::text").extract()[0]
+
+        article = response.xpath("//div[1]/section[2]/div[1]/div/article/div[3]/p").css("::text").extract()
+        if article == []:
+            article = response.xpath("//p").css("::text").extract()
+
+        article = [i.replace('\n', '') for i in article]  # newline characters replaced with ''
+        article = ' '.join(article)  # Convert list to string
+        article = article.lower()  # convert to lower case
+
+        words = article.split()  # Split article by whitespace into words
+
+        # remove punctuation from each word
+        table = str.maketrans('  ', '  ', string.punctuation)
+        stripped = [w.translate(table) for w in words]
+        article = ' '.join(stripped)
+
+        article = article.encode(encoding='ascii', errors='ignore')  # Encoding article text in
+
+        items['article'] = article
+
+        dateandtime = response.css(".arttidate::text").extract()[0] # single item list of string
+        dateandtime = dateandtime.split("  ")[0]        # removing "   | Source:"
+
+        if 'Last Updated : ' in dateandtime:
+            dateandtime = dateandtime.replace('Last Updated : ', '')
+        if ' IST' in dateandtime:
+            dateandtime = dateandtime.replace(' IST', '')
+
+        datetime_object = datetime.strptime(dateandtime, '%b %d, %Y %I:%M %p')  # Scraped string to datetime object
+        date = datetime_object.strftime("%d-%m-%Y")  # DD-MM-YY
+        time = datetime_object.strftime("%H:%M")  # HH:MM
+
+        items['date'] = date
+        items['time'] = time
+
+        try:
+            stockdata = get_history(symbol=items['stockname'], start=datetime_object.date(), end=datetime_object.date())
+            items['close'] = stockdata['Close'][0]
+            items['prevclose'] = stockdata['Prev Close'][0]
+        except:
+            items['close'] = 'NA'
+            items['prevclose'] = 'NA'
+
+        items['ztemp']=''
+        yield items
--- scrape.py
+++ scrape.py
@@ -1,15 +1,18 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+import pandas as pd
 
 process = CrawlerProcess(get_project_settings())
 
-# 'economictimes' and 'moneycontrol' is the name of one of the spiders of the project
+websites = pd.read_excel('input.xlsx', sheet_name='input')['WEBSITE']
+websites = websites.dropna().tolist()
 
-spider = input('enter news site ET or MC : ')
-if spider == 'ET':
+if 'economictimes' in websites:
     process.crawl('economictimes')  # run economictimes scraper
-elif spider == 'MC':
+
+if 'moneycontrol' in websites:
     process.crawl('moneycontrol')   # run moneycontrol scraper
+
 process.start(stop_after_crawl=True)  # the script will block here until the crawling is finished
 
 ignore = input('\n\nData Scraped. Press Enter to continue.\n\n')
